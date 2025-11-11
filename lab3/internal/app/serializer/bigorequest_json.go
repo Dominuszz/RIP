1e39440e62657b668b20f90abbe3e43936696705
Dominuszz <127459753+Dominuszz@users.noreply.github.com>
2025-11-11 15:42:15 +0300

    date change

--- lab3/internal/app/serializer/bigorequest_json.go
+++ lab3/internal/app/serializer/bigorequest_json.go
@@ -1,29 +1,54 @@
 package serializer
 
 import (
+	"encoding/json"
 	"lab3/internal/app/ds"
 	"time"
 )
 
+type RuTime time.Time
+
+func (t RuTime) MarshalJSON() ([]byte, error) {
+	// Формат: 31.12.2025 23:59:59
+	formatted := time.Time(t).Format("02.01.2006 15:04:05")
+	return json.Marshal(formatted)
+}
+
+func (t *RuTime) UnmarshalJSON(data []byte) error {
+	var str string
+	if err := json.Unmarshal(data, &str); err != nil {
+		return err
+	}
+	parsed, err := time.Parse("02.01.2006 15:04:05", str)
+	if err != nil {
+		return err
+	}
+	*t = RuTime(parsed)
+	return nil
+}
+
+// Модифицированная структура для JSON
 type BigORequestJSON struct {
-	ID                   uint       `json:"bigo_request_id"`
-	Status               string     `json:"status"`
-	DateCreate           time.Time  `json:"date_create"`
-	Creator_Login        string     `json:"creator_login"`
-	Moderator_Login      *string    `json:"moderator_login"`
-	DateUpdate           *time.Time `json:"date_update"`
-	DateFinish           *time.Time `json:"date_finish"`
-	CalculatedTime       float64    `json:"calculated_time"`
-	CalculatedComplexity string     `json:"calculated_complexity"`
+	ID                   uint    `json:"bigo_request_id"`
+	Status               string  `json:"status"`
+	DateCreate           RuTime  `json:"date_create"`
+	Creator_Login        string  `json:"creator_login"`
+	Moderator_Login      *string `json:"moderator_login"`
+	DateUpdate           *RuTime `json:"date_update"`
+	DateFinish           *RuTime `json:"date_finish"`
+	CalculatedTime       float64 `json:"calculated_time"`
+	CalculatedComplexity string  `json:"calculated_complexity"`
 }
 
 func BigORequestToJSON(bigorequest ds.BigORequest, creator_login string, moderator_login string) BigORequestJSON {
-	var upd_date, fin_date *time.Time
+	var upd_date, fin_date *RuTime
 	if bigorequest.DateUpdate.Valid {
-		upd_date = &bigorequest.DateUpdate.Time
+		tmp := RuTime(bigorequest.DateUpdate.Time)
+		upd_date = &tmp
 	}
 	if bigorequest.DateFinish.Valid {
-		fin_date = &bigorequest.DateFinish.Time
+		tmp := RuTime(bigorequest.DateFinish.Time)
+		fin_date = &tmp
 	}
 	var m_login *string
 	if moderator_login != "" {
@@ -33,7 +58,7 @@ func BigORequestToJSON(bigorequest ds.BigORequest, creator_login string, moderat
 	return BigORequestJSON{
 		ID:                   bigorequest.ID,
 		Status:               bigorequest.Status,
-		DateCreate:           bigorequest.DateCreate,
+		DateCreate:           RuTime(bigorequest.DateCreate),
 		Creator_Login:        creator_login,
 		Moderator_Login:      m_login,
 		DateUpdate:           upd_date,
--- lab3/internal/app/serializer/complexclass_json.go
+++ lab3/internal/app/serializer/complexclass_json.go
@@ -4,6 +4,7 @@ import "lab3/internal/app/ds"
 
 type ComplexClassJSON struct {
 	ID          uint    `json:"compclass_id"`
+	IMG         string  `json:"img"`
 	Complexity  string  `json:"complexity"`
 	Degree      float64 `json:"degree"`
 	DegreeText  string  `json:"degree_text"`
@@ -14,6 +15,7 @@ type ComplexClassJSON struct {
 func CompClassToJSON(complexclass ds.ComplexClass) ComplexClassJSON {
 	return ComplexClassJSON{
 		ID:          complexclass.ID,
+		IMG:         complexclass.IMG,
 		Complexity:  complexclass.Complexity,
 		Degree:      complexclass.Degree,
 		DegreeText:  complexclass.DegreeText,
@@ -24,6 +26,7 @@ func CompClassToJSON(complexclass ds.ComplexClass) ComplexClassJSON {
 
 func CompClassFromJSON(compclassJSON ComplexClassJSON) ds.ComplexClass {
 	return ds.ComplexClass{
+		IMG:         compclassJSON.IMG,
 		Complexity:  compclassJSON.Complexity,
 		Degree:      compclassJSON.Degree,
 		DegreeText:  compclassJSON.DegreeText,
